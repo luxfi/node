ba2117a21f23db70550a98b6db6a016686c3ca86
Zach Kelling <z@zeekay.io>
2022-10-31 17:44:44 -0400

    Update getters, constants

--- ids/node_id_test.go
+++ ids/node_id_test.go
@@ -65,7 +65,7 @@ func TestNodeIDMarshalJSON(t *testing.T) {
 	}{
 		{"NodeID{}", NodeID{}, []byte("\"NodeID-111111111111111111116DBWJs\""), nil},
 		{
-			"ID(\"ava labs\")",
+			"ID(\"lux labs\")",
 			NodeID{'a', 'v', 'a', ' ', 'l', 'a', 'b', 's'},
 			[]byte("\"NodeID-9tLMkeWFhWXd8QZc4rSiS5meuVXF5kRsz\""),
 			nil,
@@ -92,7 +92,7 @@ func TestNodeIDUnmarshalJSON(t *testing.T) {
 	}{
 		{"NodeID{}", []byte("null"), NodeID{}, false},
 		{
-			"NodeID(\"ava labs\")",
+			"NodeID(\"lux labs\")",
 			[]byte("\"NodeID-9tLMkeWFhWXd8QZc4rSiS5meuVXF5kRsz\""),
 			NodeID{'a', 'v', 'a', ' ', 'l', 'a', 'b', 's'},
 			false,
