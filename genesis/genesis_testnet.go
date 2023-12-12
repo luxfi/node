9bec13449fca73adf4d34a717df8610a1074f4a0
Zach Kelling <z@zeekay.io>
2023-12-12 15:49:10 +0100

    Add back in the testnet genesis

---
+++ genesis/genesis_testnet.go
@@ -0,0 +1,48 @@
+// Copyright (C) 2019-2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package genesis
+
+import (
+	"time"
+
+	_ "embed"
+
+	"github.com/luxdefi/node/utils/units"
+	"github.com/luxdefi/node/vms/platformvm/reward"
+)
+
+var (
+	//go:embed genesis_fuji.json
+	fujiGenesisConfigJSON []byte
+
+	// FujiParams are the params used for the fuji testnet
+	FujiParams = Params{
+		TxFeeConfig: TxFeeConfig{
+			TxFee:                         units.MilliLux,
+			CreateAssetTxFee:              10 * units.MilliLux,
+			CreateSubnetTxFee:             100 * units.MilliLux,
+			TransformSubnetTxFee:          1 * units.Lux,
+			CreateBlockchainTxFee:         100 * units.MilliLux,
+			AddPrimaryNetworkValidatorFee: 0,
+			AddPrimaryNetworkDelegatorFee: 0,
+			AddSubnetValidatorFee:         units.MilliLux,
+			AddSubnetDelegatorFee:         units.MilliLux,
+		},
+		StakingConfig: StakingConfig{
+			UptimeRequirement: .8, // 80%
+			MinValidatorStake: 1 * units.Lux,
+			MaxValidatorStake: 3 * units.MegaLux,
+			MinDelegatorStake: 1 * units.Lux,
+			MinDelegationFee:  20000, // 2%
+			MinStakeDuration:  24 * time.Hour,
+			MaxStakeDuration:  365 * 24 * time.Hour,
+			RewardConfig: reward.Config{
+				MaxConsumptionRate: .12 * reward.PercentDenominator,
+				MinConsumptionRate: .10 * reward.PercentDenominator,
+				MintingPeriod:      365 * 24 * time.Hour,
+				SupplyCap:          720 * units.MegaLux,
+			},
+		},
+	}
+)
