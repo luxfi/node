e8d1321bffb6a8d4645298b1504a1aa0ddb35856
Zach Kelling <z@zeekay.io>
2023-12-17 01:10:50 +0100

    Update deps

--- trace/noop.go
+++ trace/noop.go
@@ -7,21 +7,22 @@ import (
 	"context"
 
 	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/noop"
 
 	"github.com/luxdefi/node/utils/constants"
 )
 
-var Noop Tracer = noOpTracer{
-	t: trace.NewNoopTracerProvider().Tracer(constants.AppName),
-}
-
 // noOpTracer is an implementation of trace.Tracer that does nothing.
 type noOpTracer struct {
-	t trace.Tracer
+	trace.Tracer
+}
+
+var Noop Tracer = noOpTracer{
+	Tracer: noop.NewTracerProvider().Tracer(constants.AppName),
 }
 
 func (n noOpTracer) Start(ctx context.Context, spanName string, opts ...trace.SpanStartOption) (context.Context, trace.Span) {
-	return n.t.Start(ctx, spanName, opts...)
+	return n.Start(ctx, spanName, opts...)
 }
 
 func (noOpTracer) Close() error {
