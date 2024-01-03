aece65b94ee6c100201c0de5ebf37c2a6a0029de
Zach Kelling <z@zeekay.io>
2024-01-03 10:16:56 +0100

    Update NoopTracer

--- trace/noop.go
+++ trace/noop.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2023, Lux Partners Limited. All rights reserved.
+// Copyright (C) 2021-2024, Lux Partners Limited. All rights reserved.
 // See the file LICENSE for licensing terms.
 
 package trace
@@ -7,22 +7,21 @@ import (
 	"context"
 
 	"go.opentelemetry.io/otel/trace"
-	"go.opentelemetry.io/otel/trace/noop"
 
 	"github.com/luxdefi/node/utils/constants"
 )
 
-// noOpTracer is an implementation of trace.Tracer that does nothing.
-type noOpTracer struct {
-	trace.Tracer
+var Noop Tracer = noOpTracer{
+	t: trace.NewNoopTracerProvider().Tracer(constants.AppName),
 }
 
-var Noop Tracer = noOpTracer{
-	Tracer: noop.NewTracerProvider().Tracer(constants.AppName),
+// noOpTracer is an implementation of trace.Tracer that does nothing.
+type noOpTracer struct {
+	t trace.Tracer
 }
 
 func (n noOpTracer) Start(ctx context.Context, spanName string, opts ...trace.SpanStartOption) (context.Context, trace.Span) {
-	return n.Start(ctx, spanName, opts...)
+	return n.t.Start(ctx, spanName, opts...)
 }
 
 func (noOpTracer) Close() error {
