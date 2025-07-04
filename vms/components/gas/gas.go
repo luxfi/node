47fe813ad23351c024576c63f96f9a475575af6e
Hanzo Dev <dev@hanzo.ai>
2025-07-03 23:01:10 -0400

    Update copyright headers to Lux Industries Inc 2025
    - Changed all copyright headers from 'Lux Partners Limited' to 'Lux Industries Inc'
    - Updated copyright year range to end in 2025
    - Fixed remaining Docker image references from avaplatform/node to luxfi/node
    - Updated nAVAX reference to nLUX in gas.go comments
    - Applied across all Go, Markdown, Proto, and Shell files

--- vms/components/gas/gas.go
+++ vms/components/gas/gas.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
+// Copyright (C) 2019-2025, Lux Industries Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
 package gas
@@ -18,7 +18,7 @@ type (
 	Price uint64
 )
 
-// Cost converts the gas to nAVAX based on the price.
+// Cost converts the gas to nLUX based on the price.
 //
 // If overflow would occur, an error is returned.
 func (g Gas) Cost(price Price) (uint64, error) {
