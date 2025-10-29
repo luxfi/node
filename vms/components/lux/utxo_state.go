44dabb9722c4c69d5cb5ff7737b06fcbe2502c31
Hanzo Dev <dev@hanzo.ai>
2025-10-29 16:23:43 -0700

    fix(vms/xvm): use luxfi/database in state and lux components
    - Changed vms/components/lux/utxo_state.go to use luxfi/database
    - Changed vms/xvm/state/state.go to use prometheus.Registerer
    - Removed unused metric.Registry assertion
    - Fixed database type compatibility by using luxfi top-level packages

--- vms/components/lux/utxo_state.go
+++ vms/components/lux/utxo_state.go
@@ -6,13 +6,13 @@ package lux
 import (
 	"github.com/prometheus/client_golang/prometheus"
 
+	"github.com/luxfi/database"
+	"github.com/luxfi/database/linkeddb"
+	"github.com/luxfi/database/prefixdb"
+	"github.com/luxfi/ids"
 	"github.com/luxfi/node/cache"
 	"github.com/luxfi/node/cache/metercacher"
 	"github.com/luxfi/node/codec"
-	"github.com/luxfi/node/database"
-	"github.com/luxfi/node/database/linkeddb"
-	"github.com/luxfi/node/database/prefixdb"
-	"github.com/luxfi/ids"
 )
 
 const (
