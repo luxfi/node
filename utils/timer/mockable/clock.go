682b48f4b7390b23dd315dec85778ddfe07c8ae0
Zach Kelling <z@zeekay.ai>
2025-12-11 13:06:31 -0800

    fix: add mutex to mockable.Clock for thread-safety
    Clock.Set() and Clock.Time() can race when called from different
    goroutines. This adds a sync.RWMutex to protect concurrent access.

--- utils/timer/mockable/clock.go
+++ utils/timer/mockable/clock.go
@@ -3,25 +3,41 @@
 
 package mockable
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // MaxTime was taken from https://stackoverflow.com/questions/25065055/what-is-the-maximum-time-time-in-go/32620397#32620397
 var MaxTime = time.Unix(1<<63-62135596801, 0) // 0 is used because we drop the nano-seconds
 
-// Clock acts as a thin wrapper around global time that allows for easy testing
+// Clock acts as a thin wrapper around global time that allows for easy testing.
+// It is safe for concurrent use.
 type Clock struct {
+	mu    sync.RWMutex
 	faked bool
 	time  time.Time
 }
 
 // Set the time on the clock
-func (c *Clock) Set(time time.Time) { c.faked = true; c.time = time }
+func (c *Clock) Set(t time.Time) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.faked = true
+	c.time = t
+}
 
 // Sync this clock with global time
-func (c *Clock) Sync() { c.faked = false }
+func (c *Clock) Sync() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.faked = false
+}
 
 // Time returns the time on this clock
 func (c *Clock) Time() time.Time {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	if c.faked {
 		return c.time
 	}
