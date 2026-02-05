65bb3e01c5ba55f5ffbbce9f656347dcda7d4029
Zach Kelling <z@zeekay.io>
2026-02-04 23:04:18 -0800

    chore: standardize copyright to Lux Industries Inc.
    - Update Lux Partners Limited -> Lux Industries Inc.
    - Update 2019-2024 -> 2019-2025
    - Remove comma in "Lux Industries, Inc." -> "Lux Industries Inc."

--- vms/rpcchainvm/gvalidators/gvalidators_client.go
+++ vms/rpcchainvm/gvalidators/gvalidators_client.go
@@ -1,6 +1,6 @@
 //go:build grpc
 
-// Copyright (C) 2019-2025, Lux Industries, Inc. All rights reserved.
+// Copyright (C) 2019-2025, Lux Industries Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
 package gvalidators
