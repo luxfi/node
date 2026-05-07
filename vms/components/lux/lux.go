f6cd97b7fbc0b39b6fa54cd3d4b15840197227d6
Zach Kelling <z@zeekay.io>
2026-05-06 21:09:12 -0700

    .gitignore + commit vms/components/lux content
    Anchor /lux + /luxd binary patterns at repo root so the gitignore
    rule doesn't accidentally match every directory called "lux" deep
    in the tree (which was excluding vms/components/lux that the
    previous commit tried to land).
    
    Add the actual UTXO type tree files so v1.26.5 ships with the
    package contents external consumers (liquidity/network-bootstrap
    etc) need.

---
+++ vms/components/lux/lux.go
@@ -0,0 +1,10 @@
+// Copyright (C) 2019-2025, Lux Industries Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package lux
+
+// This file is kept minimal as types are defined in their respective files:
+// - UTXO is defined in utxo.go
+// - UTXOID is defined in utxo_id.go
+// - Asset is defined in asset.go
+// - TransferableInput and TransferableOutput are defined in transferables.go
